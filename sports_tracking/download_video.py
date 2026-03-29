3315842ae20c77990ac88d6f19e95c7a2c9f5399
ridhima <118359424+Ridhima35@users.noreply.github.com>
2026-03-29 21:14:15 +0530

    Delete sports_tracking directory

--- sports_tracking/download_video.py
+++
@@ -1,5 +0,0 @@
-import urllib.request
-url = 'https://github.com/intel-iot-devkit/sample-videos/raw/master/person-bicycle-car-detection.mp4'
-print(f"Downloading from {url}...")
-urllib.request.urlretrieve(url, 'input.mp4')
-print('Downloaded input.mp4 successfully.')
